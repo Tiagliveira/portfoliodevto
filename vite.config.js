6082cc4b7c707eb46c35e674b00d0d88e16e3b05
Tiagliveira <tiagliveira92@gmail.com>
2025-08-12 19:01:51 -0300

    Corrige base para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Portf-lioDevTO',
+  base: '/Portf-lioDevTO/', // 🔹 Barra no final é essencial
 })
